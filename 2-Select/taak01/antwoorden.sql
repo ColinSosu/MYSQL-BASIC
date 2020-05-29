cb109c53d23122fdb126ea80c20227ce2cd9a7fa
Colin <coli-nm@hotmail.com>
2020-05-29 04:19:18 +0200

    taak 2

--- 2-Select/taak01/antwoorden.sql
+++ 2-Select/taak01/antwoorden.sql
@@ -1,6 +1,207 @@
 -- Opdracht 1 - voorbeeld
 SELECT * FROM jaar2016
+Denmark
+Western Europe
+1
+7526
+Switzerland
+Western Europe
+2
+7509
+Iceland
+Western Europe
+3
+7501
+Norway
+Western Europe
+4
+7498
+Finland
+Western Europe
+5
+7413
+Canada
+North America
+6
+7404
+Netherlands
+Western Europe
+7
+7339
+New Zealand
+Australia and New Zealand
+8
+7334
+Australia
+Australia and New Zealand
+9
+7313
+Sweden
+Western Europe
+10
+7291
+Israel
+Middle East and Northern Africa
+11
+7267
+Austria
+Western Europe
+12
+7119
+United States
+North America
+13
+7104
+Costa Rica
+Latin America and Caribbean
+14
+7087
+Puerto Rico
+Latin America and Caribbean
+15
+7039
+Germany
+Western Europe
+16
+6994
+Brazil
+Latin America and Caribbean
+17
+6952
+Belgium
+Western Europe
+18
+6929
+Ireland
+Western Europe
+19
+6907
+Luxembourg
+Western Europe
+20
+6871
+Mexico
+Latin America and Caribbean
+21
+6778
+Singapore
+Southeastern Asia
+22
+6739
+United Kingdom
+Western Europe
+23
+6725
+Chile
+Latin America and Caribbean
+24
+6705
+Panama
+Latin America and Caribbean
+25
+6701
+
 -- Opdracht 2 
+Switzerland
+Western Europe
+1
+7587
+Iceland
+Western Europe
+2
+7561
+Denmark
+Western Europe
+3
+7527
+Norway
+Western Europe
+4
+7522
+Canada
+North America
+5
+7427
+Finland
+Western Europe
+6
+7406
+Netherlands
+Western Europe
+7
+7378
+Sweden
+Western Europe
+8
+7364
+New Zealand
+Australia and New Zealand
+9
+7286
+Australia
+Australia and New Zealand
+10
+7284
+Israel
+Middle East and Northern Africa
+11
+7278
+Costa Rica
+Latin America and Caribbean
+12
+7226
+Austria
+Western Europe
+13
+72
+Mexico
+Latin America and Caribbean
+14
+7187
+United States
+North America
+15
+7119
+Brazil
+Latin America and Caribbean
+16
+6983
+Luxembourg
+Western Europe
+17
+6946
+Ireland
+Western Europe
+18
+694
+Belgium
+Western Europe
+19
+6937
+United Arab Emirates
+Middle East and Northern Africa
+20
+6901
+United Kingdom
+Western Europe
+21
+6867
+Oman
+Middle East and Northern Africa
+22
+6853
+Venezuela
+Latin America and Caribbean
+23
+681
+Singapore
+Southeastern Asia
+24
+6798
+Panama
+Latin America and Caribbean
+25
+6786
 
 -- Opdracht 3
 
